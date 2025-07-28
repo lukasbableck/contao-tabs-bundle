8195047ed5a2d1240c3472b87669cf69f66b9a37
Lukas Bableck <lukas@bableck.dev>
2025-07-28 13:32:00 +0200

    DCA fields, keyboard navigation, cleanup

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,55 +1,55 @@
 import '../css/tabs.pcss';
 
 class A11yTabs {
-	constructor() {
+	constructor(element) {
+		this.element = element;
 		this.tabs = [];
 		this.activeTabIndex = 0;
-		this.init();
-	}
-	init() {
-		const tabElements = document.querySelectorAll('[role="tab"]');
-		this.tabs = Array.from(tabElements).map((tab, index) => {
+		this.orientation = this.element.querySelector('[role="tablist"]').getAttribute('aria-orientation') || 'horizontal';
+		this.tabs = Array.from(this.element.querySelectorAll('[role="tab"]')).map((tab, index) => {
 			return {
 				id: tab.id,
 				isActive: index === 0,
 				element: tab,
 			};
 		});
-		this.tabs.forEach((tab, index) => {
-			tab.element.addEventListener('click', () => {
-				this.setActiveTab(tab.id);
-			});
-			tab.element.addEventListener('keydown', (event) => {
-				if (event.key === 'Enter' || event.key === ' ') {
-					this.setActiveTab(tab.id);
-					event.preventDefault();
-				} else if (event.key === 'ArrowLeft' || event.key === 'ArrowUp') {
-					if (index === 0) {
-						return;
-					}
-					this.previousTab();
-					tab.element.previousElementSibling.focus();
-					event.preventDefault();
-				} else if (event.key === 'ArrowRight' || event.key === 'ArrowDown') {
-					if (index === this.tabs.length - 1) {
-						return;
-					}
-					this.nextTab();
-					tab.element.nextElementSibling.focus();
-					event.preventDefault();
-				}
-			});
+		this.tabs.forEach((tab) => {
+			this.bindEvents(tab);
 		});
 		this.activeTabIndex = this.tabs.findIndex((tab) => tab.isActive);
 		this.updateTabVisibility();
 	}
+
+	bindEvents(tab) {
+		tab.element.addEventListener('click', () => {
+			this.setActiveTab(tab);
+		});
+		tab.element.addEventListener('keydown', (event) => {
+			if (this.orientation == 'vertical' ? event.key == 'ArrowUp' : event.key == 'ArrowLeft') {
+				this.previousTab();
+			} else if (this.orientation == 'vertical' ? event.key == 'ArrowDown' : event.key == 'ArrowRight') {
+				this.nextTab();
+			} else if (event.key === 'Home') {
+				this.setActiveTab(this.tabs[0]);
+			} else if (event.key === 'End') {
+				this.setActiveTab(this.tabs[this.tabs.length - 1]);
+			}
+
+			if ((this.orientation == 'vertical' ? event.key == 'ArrowUp' : event.key == 'ArrowLeft') || (this.orientation == 'vertical' ? event.key == 'ArrowDown' : event.key == 'ArrowRight') || event.key == 'Home' || event.key == 'End') {
+				this.focusActiveTab();
+				event.preventDefault();
+			}
+		});
+	}
+
 	updateTabVisibility() {
-		this.tabs.forEach((tab, index) => {
+		this.tabs.forEach((tab) => {
+			tab.element.setAttribute('tabindex', tab.isActive ? '0' : '-1');
 			tab.element.setAttribute('aria-selected', tab.isActive);
 		});
 		const activeTabContent = document.querySelector(`[role="tabpanel"][aria-labelledby="${this.tabs[this.activeTabIndex].id}"]`);
 		if (activeTabContent) {
-			activeTabContent.removeAttribute('tabIndex');
+			activeTabContent.tabIndex = 0;
 			activeTabContent.classList.remove('hidden');
 		}
 		const inactiveTabs = this.tabs.filter((t) => !t.isActive);
@@ -61,24 +61,39 @@ class A11yTabs {
 			}
 		});
 	}
-	setActiveTab(tabID) {
-		const tab = this.tabs.find((t) => t.id === tabID);
-		if (tab) {
-			this.activeTabIndex = this.tabs.indexOf(tab);
-			this.tabs.forEach((t, index) => {
-				t.isActive = index === this.activeTabIndex;
-			});
-			this.updateTabVisibility();
-		}
+
+	setActiveTab(tab) {
+		this.activeTabIndex = this.tabs.indexOf(tab);
+		this.tabs.forEach((t, index) => {
+			if (index === this.activeTabIndex) {
+				t.isActive = true;
+			} else {
+				t.isActive = false;
+			}
+		});
+		this.updateTabVisibility();
 	}
+
 	previousTab() {
 		if (this.activeTabIndex > 0) {
-			this.setActiveTab(this.tabs[this.activeTabIndex - 1].id);
+			this.setActiveTab(this.tabs[this.activeTabIndex - 1]);
+		} else {
+			this.setActiveTab(this.tabs[this.tabs.length - 1]);
 		}
 	}
+
 	nextTab() {
 		if (this.activeTabIndex < this.tabs.length - 1) {
-			this.setActiveTab(this.tabs[this.activeTabIndex + 1].id);
+			this.setActiveTab(this.tabs[this.activeTabIndex + 1]);
+		} else {
+			this.setActiveTab(this.tabs[0]);
+		}
+	}
+
+	focusActiveTab() {
+		const activeTab = this.tabs[this.activeTabIndex];
+		if (activeTab && activeTab.element) {
+			activeTab.element.focus();
 		}
 	}
 }
---
+++ contao/dca/tl_content.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['tabs'] = '{type_legend},type,headline;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend},invisible,start,stop';
